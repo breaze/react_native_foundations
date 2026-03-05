713e4c9e5262f24f3598f9363e06be8632cc62a2
breaze <simon.gaviriao@gmail.com>
2026-03-04 19:08:16 -0500

    Modales

--- app/(tabs)/(inicio)/index.tsx
+++ app/(tabs)/(inicio)/index.tsx
@@ -1,7 +1,10 @@
 import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 export default function Index() {
   const router = useRouter();
+  const [visible, setVisible] = useState(false);
+  const [nombre, setNombre] = useState('');
   return (
     <View
       style={{
@@ -22,6 +25,58 @@ export default function Index() {
         <Text>Redireccionar</Text>
       </TouchableOpacity>
 
+      <TouchableOpacity
+        onPress={() => { setVisible(true) }}
+      >
+        <Text>Modal</Text>
+      </TouchableOpacity>
+      <Modal
+        visible={visible}
+        transparent={true}
+        onRequestClose={() => setVisible(false)}
+        animationType="fade"
+      >
+        <View style={styles.modalPrincipal}>
+          <View style={styles.contenidoModal}>
+            <Text>Este es el modal</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre"
+              value={nombre}
+              onChangeText={setNombre}
+            />
+            <TouchableOpacity
+              onPress={() => { setVisible(false) }}
+            >
+              <Text>Cerrar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <Text>{nombre}</Text>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalPrincipal: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contenidoModal: {
+    width: 300,
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 10
+  },
+  input: {
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
